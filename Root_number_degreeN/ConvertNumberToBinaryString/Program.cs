3a7f6570ad9dd259c81127031b07e43ed7426125
Sinitsyn <sim-85@mail.ru>
2016-04-18 17:04:24 +0300

    Своя реализация нахождения вещественной степени из числа методом Ньютона

---
+++ Root_number_degreeN/ConvertNumberToBinaryString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace ConvertNumberToBinaryString
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите неотрицательное целое число для преобразования");
+			uint number;
+			while (!(UInt32.TryParse(Console.ReadLine(), out number)))
+				Console.WriteLine("Некорректно введено число для преобразования. Введите еще раз");
+			Console.WriteLine("{0} - значение методом 'Convert.ToString'", Convert.ToString(number, 2));
+			Console.WriteLine("{0} - значение нашим способом", ConvertNumberToBinaryString(number));
+			Console.ReadLine();
+		}
+
+		public static string ConvertNumberToBinaryString(uint A)
+		{
+			if (A == 0) return "0";
+			int bitLength = 1;
+			uint B = A;
+			while ((B >>= 1) != 0)
+				bitLength++;
+			char[] charArr = new char[bitLength];
+			B = 1;
+			for (int i = bitLength - 1; i >= 0; i--, B <<= 1)
+				charArr[i] = ((A & B) == 0) ? '0' : '1';
+			return new string(charArr);
+		}
+	}
+}
---
+++ Root_number_degreeN/Root_number_degreeN/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Root_number_degreeN
+{
+	class Program
+	{
+		const double epsilon = 0.00001;
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите значение числа А");
+			double A;
+			while (!(double.TryParse(Console.ReadLine(), out A)))
+				Console.WriteLine("Некорректно введено число А. Введите еще раз");
+
+			Console.WriteLine("Введите значение извлекаемого корня N");
+			double N;
+			while (!(double.TryParse(Console.ReadLine(), out N)))
+				Console.WriteLine("Некорректно введено число N. Введите еще раз");
+
+			PrintResult(MyMath.Root(N, A, epsilon), Math.Pow(A, (1 / N)));
+
+			Console.ReadKey();
+		}
+
+		public static void PrintResult(double myResult, double netResult)
+		{
+			Console.WriteLine("Моим методом: {0}. Методом Math.Pow: {1}", myResult, netResult);
+			Console.WriteLine("Погрешность результата между машинным и ручным методом: {0}", Math.Abs(myResult - netResult));
+		}
+		
+	}
+
+	public class MyMath
+	{
+		public static double Root(double N, double A, double eps)
+		{
+			var x0 = 1.0; //Начальное предположение, примем равным числу 1
+			var x1 = (1 / N) * ((N - 1) * x0 + A / Math.Pow(x0, N - 1));
+
+			while (Math.Abs(x1 - x0) > eps)
+			{
+				x0 = x1;
+				x1 = (1 / N) * ((N - 1) * x0 + A / Math.Pow(x0, N - 1));
+			}
+			return x1;
+		}
+	}
+}
